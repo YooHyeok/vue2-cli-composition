344d8ac0fde74be1b97d8fad827aede97ddf6940
webdevyoo <yjk7454@naver.com>
2024-12-13 05:27:39 +0900

    feat: `main.js` - composition api 모듈 import 및 Vue.use() 플러그인 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueCompositionAPI from '@vue/composition-api';
 
+Vue.use(VueCompositionAPI); // npm install 후 전역 등록
 Vue.config.productionTip = false
 
 new Vue({
